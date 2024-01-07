61a57516846f49f332b84b89382fb93acc26230b
Chetan Tyagi <87710619+chetanty@users.noreply.github.com>
2024-01-07 06:34:09 -0700

    Update activity_page3.xml

--- app/src/main/java/com/example/hc/Page1.java
+++ app/src/main/java/com/example/hc/Page1.java
@@ -1,14 +1,146 @@
 package com.example.hc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.datastore.MapDataStore;
+import org.mapsforge.map.layer.Layer;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.overlay.Marker;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
 
-public class Page1 extends AppCompatActivity {
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URI;
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.core.model.MapPosition;
+
+
+/**
+ * A very basic Android app example.
+ * <p>
+ * You'll need a map with filename alberta.map from download.mapsforge.org in device storage.
+ */
+public class
+Page1 extends Activity {
+
+    // Request code for selecting a map file
+    private static final int SELECT_MAP_FILE = 0;
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page1);
+
+        /*
+         * Before you make any calls on the mapsforge library, you need to initialize the
+         * AndroidGraphicFactory. Behind the scenes, this initialization process gathers a bit of
+         * information on your device, such as the screen resolution, that allows mapsforge to
+         * automatically adapt the rendering for the device.
+         * If you forget this step, your app will crash. You can place this code, like in the
+         * Samples app, in the Android Application class. This ensures it is created before any
+         * specific activity. But it can also be created in the onCreate() method in your activity.
+         */
+        AndroidGraphicFactory.createInstance(getApplication());
+
+        /*
+         * A MapView is an Android View (or ViewGroup) that displays a mapsforge map. You can have
+         * multiple MapViews in your app or even a single Activity. Have a look at the mapviewer.xml
+         * on how to create a MapView using the Android XML Layout definitions. Here we create a
+         * MapView on the fly and make the content view of the activity the MapView. This means
+         * that no other elements make up the content of this activity.
+         */
+        mapView = new MapView(this);
+        setContentView(mapView);
+
+        /*
+         * Open map.
+         */
+        Intent intent = new Intent(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ? Intent.ACTION_OPEN_DOCUMENT : Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, SELECT_MAP_FILE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_MAP_FILE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+
+                Uri uri = data.getData();
+                openMap(uri);
+            }
+        }
+    }
+
+    private void openMap(Uri uri) {
+        try {
+            /*
+             * We then make some simple adjustments, such as showing a scale bar and zoom controls.
+             */
+            mapView.getMapScaleBar().setVisible(true);
+            mapView.setBuiltInZoomControls(true);
+
+            /*
+             * To avoid redrawing all the tiles all the time, we need to set up a tile cache with an
+             * utility method.
+             */
+            TileCache tileCache = AndroidUtil.createTileCache(this, "mapcache",
+                    mapView.getModel().displayModel.getTileSize(), 1f,
+                    mapView.getModel().frameBufferModel.getOverdrawFactor());
+
+            /*
+             * Now we need to set up the process of displaying a map. A map can have several layers,
+             * stacked on top of each other. A layer can be a map or some visual elements, such as
+             * markers. Here we only show a map based on a mapsforge map file. For this we need a
+             * TileRendererLayer. A TileRendererLayer needs a TileCache to hold the generated map
+             * tiles, a map file from which the tiles are generated and Rendertheme that defines the
+             * appearance of the map.
+             */
+            FileInputStream fis = (FileInputStream) getContentResolver().openInputStream(uri);
+            MapDataStore mapDataStore = new MapFile(fis);
+            TileRendererLayer tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
+                    mapView.getModel().mapViewPosition, AndroidGraphicFactory.INSTANCE);
+            tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.DEFAULT);
+
+            /*
+             * On its own a tileRendererLayer does not know where to display the map, so we need to
+             * associate it with our mapView.
+             */
+            mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+            /*
+             * The map also needs to know which area to display and at what zoom level.
+             * Note: this map position is specific to Edmonton area.
+             */
+            mapView.setCenter(new LatLong(53.5461, -113.4938));
+            mapView.setZoomLevel((byte) 12);
+        } catch (Exception e) {
+            /*
+             * In case of map file errors avoid crash, but developers should handle these cases!
+             */
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        /*
+         * Whenever your activity exits, some cleanup operations have to be performed lest your app
+         * runs out of memory.
+         */
+        mapView.destroyAll();
+        AndroidGraphicFactory.clearResourceMemoryCache();
+        super.onDestroy();
     }
 }
