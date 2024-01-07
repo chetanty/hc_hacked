beac81577a7a84842770e38cffc66a9a1ae9bdbf
Chetan Tyagi <ctat684@gmail.com>
2024-01-07 09:45:52 -0700

    hi

--- app/src/main/java/com/example/hc/Page1.java
+++ app/src/main/java/com/example/hc/Page1.java
@@ -1,8 +1,8 @@
 package com.example.hc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class Page1 extends AppCompatActivity {
 
@@ -10,5 +10,8 @@ public class Page1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+
+        PDFView pdfView = findViewById(R.id.pdfView); // Make sure you have a PDFView in your layout
+        pdfView.fromAsset("d.pdf").load();
     }
 }
